59027edd55f815e800433a7e5ced33414c34aaa7
littledarkbug <didisossou@gmail.com>
2022-02-26 09:49:43 +0000

    add Cours-Details and Projet-details

---
+++ src/pages/CoursDetails/index.js
@@ -0,0 +1,61 @@
+// @mui material components
+import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+
+// Material Kit 2 React examples
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+import DefaultFooter from "examples/Footers/DefaultFooter";
+
+import Posts from "pages/CoursDetails/sections/Posts";
+
+// Routes
+import routes from "menu";
+import footerRoutes from "footer.routes";
+
+// Images
+import bgImage from "assets/images/Cours/banniere.jpg";
+
+function CoursDetails() {
+  return (
+    <>
+      <DefaultNavbar routes={routes} transparent light />
+      <MKBox bgColor="#dadaf0">
+        <MKBox
+          minHeight="25rem"
+          width="100%"
+          sx={{
+            backgroundImage: ({ functions: { linearGradient, rgba }, palette: { gradients } }) =>
+              `${linearGradient(
+                rgba(gradients.dark.main, 0.8),
+                rgba(gradients.dark.state, 0.8)
+              )}, url(${bgImage})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            display: "grid",
+            placeItems: "center",
+          }}
+        />
+        <Card
+          sx={{
+            p: 2,
+            mx: { xs: 2, lg: 3 },
+            mt: -8,
+            mb: 4,
+            backgroundColor: ({ palette: { white }, functions: { rgba } }) => rgba(white.main, 0.8),
+            backdropFilter: "saturate(200%) blur(30px)",
+            boxShadow: ({ boxShadows: { xxl } }) => xxl,
+          }}
+        >
+          <Posts />
+        </Card>
+        <MKBox pt={6} px={1} mt={6}>
+          <DefaultFooter content={footerRoutes} />
+        </MKBox>
+      </MKBox>
+    </>
+  );
+}
+
+export default CoursDetails;
